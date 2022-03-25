8d83747e713d50c23de60f7f70c89c6e49c2745b
obeespace <obeewon20@gmail.com>
2022-03-25 13:59:40 +0100

    Updates

---
+++ precache-manifest.ad46a0920ce101485bcf5084acbb1b89.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4011747987f8ec1e8a98",
+    "url": "/my-travel-journal/static/js/main.40117479.chunk.js"
+  },
+  {
+    "revision": "24c69f000997ca075680",
+    "url": "/my-travel-journal/static/js/1.24c69f00.chunk.js"
+  },
+  {
+    "revision": "84002eb5bd3c439cca0a",
+    "url": "/my-travel-journal/static/js/runtime~main.84002eb5.js"
+  },
+  {
+    "revision": "1fd15c5583a54752508e341ef042539d",
+    "url": "/my-travel-journal/index.html"
+  }
+];
